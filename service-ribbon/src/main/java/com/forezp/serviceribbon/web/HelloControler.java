69f22a3e19f6acfa627c6518ac5b9e3e4d94ecdb
yixiao1874 <1499957726@qq.com>
2018-05-30 18:12:17 +0800

    Initial commit

---
+++ service-ribbon/src/main/java/com/forezp/serviceribbon/web/HelloControler.java
@@ -0,0 +1,26 @@
+package com.forezp.serviceribbon.web;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+import com.forezp.serviceribbon.service.HelloService;
+
+@RestController
+public class HelloControler {
+
+    @Autowired
+    HelloService helloService;
+
+    @RequestMapping(value = "/hi")
+    public String hi(@RequestParam String name){
+        return helloService.hiService(name);
+    }
+
+    @RequestMapping("/hello")
+    @ResponseBody
+    public String test(){
+        return "Hello World！";
+    }
+}
